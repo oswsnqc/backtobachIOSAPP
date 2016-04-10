68813676b2c465cea8ee6133812a569422e434d0
Yisen Jing <jyisen@hotmail.com>
2016-04-09 23:45:12 -0700

    back to bach project

--- Back To Bach/ViewController.swift
+++ Back To Bach/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
 
+    
+    
+    @IBAction func startPlay(sender: AnyObject) {
+        
+        player.play()
+        
+    }
+    
+    @IBAction func stopPause(sender: AnyObject) {
+        
+        player.pause()
+    }
+    
+    @IBAction func reallyStop(sender: AnyObject) {
+        
+        player.stop()
+        player.currentTime = 0
+    }
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBAction func sliderAction(sender: AnyObject) {
+        
+        player.volume = sliderValue.value
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var audioPath = NSBundle.mainBundle().pathForResource("bach1", ofType: "mp3")
+        
+        var error: NSError? = nil
+        
+        player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath as String!), error: &error)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
